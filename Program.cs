3cd1eb18050a489fcb7ee7c064333f98274b6a2f
Nader Zaky <Nader Zaky@NADER-PC>
2023-04-26 23:24:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int[] A = new int[] { 1, 3, 6, 4, 1, 2 };
+
+//Console.Write(solution(A));
+//BinaryGap(100);
+//int[] A = new int[] { 3, 8, 9, 7, 6 };
+//ArrayRotation(A, 3);
+
+//int[] A = new int[] { 9, 3, 9, 3, 9, 7, 9 };
+//OddOccuranceINArray(A);
+
+//FrogJumb(10, 90, 30);
+using ProblemSolvingSolutions;
+using System.Diagnostics;
+
+int[] A = new int[] { -1000, 1000 };
+//TapeEquilibrium(A);
+
+
+var myArray = Algorithems.GenerateRandomArray();
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+//Summ(myArray);
+
+//Sum(new int[] { -1000, 1000});
+// Console.WriteLine(PassingCars(new int[] {0,1,0,1,1 }));
+// repeatingChar("hellooaa");
+// MaximumWordCount("Forget  CVs..Save time . x x");
+//Console.WriteLine(PascalTriangle(80));
+//ParseRomanNumerals("MCMLXI");
+//jumpingOnClouds(new int[] { 0,0,1,0,0,1,0});
+//Algorithems.repeatedString("kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm", 736778906400);
+// Algorithems.PlusOne_MySolution(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 });
+//Algorithems.DistributeCandies(new int[] {6,6,6,6 });
+//Algorithems.minimumBribes(new System.Collections.Generic.List<int> { 2,1,5,3,4 });
+// Algorithems.CanConstruct("aa", "aab");
+Algorithems.ClimbStairs(5);
+stopwatch.Stop();
+
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
+Console.ReadLine();
